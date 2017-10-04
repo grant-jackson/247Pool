0463528e76b4100d8487049514db5b049318a5ff
GarethDavidson <gareth.davidson@carfinance.co.uk>
2017-10-04 13:55:58 +0100

    leader board list

--- src/src/LeaderboardComponent/leaderboard.component.ts
+++ src/src/LeaderboardComponent/leaderboard.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+const LEADERS = [
+    { score: 150, name: 'Superman' },
+    { score: 125, name: 'Batman' },
+    { score: 105, name: 'BatGirl' },
+    { score: 103, name: 'Robin' },
+    { score: 100, name: 'Flash' }
+];
+
 @Component({
     selector: 'leaderboard-component',
     templateUrl: 'leaderboard.component.html'
 })
 
 export class LeaderboardComponent implements OnInit {
-    constructor() { }
 
+    constructor() { }
+    leaders = LEADERS;
     ngOnInit() { }
 }
