e9358a418116fd60523fd5df63b8509cd3f87dde
Grant Jackson <Grant.Jackson@uk.cf247.net>
2017-10-04 15:15:50 +0100

    added models

---
+++ src/src/providers/PlayerService.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+import { Player } from '../Models/Player';
+
+@Injectable()
+export class PlayerService {
+  GetPlayers(): Array<Player> {
+    return null;
+  }
+}
