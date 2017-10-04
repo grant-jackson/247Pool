ba6898498affa49facaa526b6d2242d468a359be
Grant Jackson <Grant.Jackson@uk.cf247.net>
2017-10-04 13:15:52 +0100

    added all of the pages and links

---
+++ src/src/ResultsComponent/results.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'results-component',
+    templateUrl: 'results.component.html'
+})
+
+export class ResultsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
